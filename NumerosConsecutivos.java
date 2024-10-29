8e4a03acdee37ce54995d809158486ecde15869b
Dionisio E. Aguilera Avila <dioagui83@gmail.com>
2024-10-29 16:43:48 +0100

    commit1

---
+++ NumerosConsecutivos.java
@@ -0,0 +1,14 @@
+
+public class NumerosConsecutivos {
+
+   
+    public static void main(String[] args) {
+   
+        for(int i = 1;i <= 10;){
+        
+            System.out.println("número "+i);
+            i++;
+        }
+    }
+    
+}
