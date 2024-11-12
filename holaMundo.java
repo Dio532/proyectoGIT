b97a0cbaf3ca2f1edf010eb52b7df20d2507e7e1
DioAgui <dioagui83@gmail.com>
2024-11-12 01:33:29 +0100

    Update holaMundo.java

--- holaMundo.java
+++ holaMundo.java
@@ -5,7 +5,7 @@ public class holaMundo {
     public static void main(String[] args) {
      
        
-          System.out.println("Hola Mundo");
+          System.out.println("Hola GIT");
           
     }
 }
